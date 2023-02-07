f130399c98f381c5f9e0a5e6f854c5ec6c531a2c
Armfath <info.armanagonou@gmail.com>
2023-02-07 08:40:14 +0100

    add unittest

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Tests for BaseModel class
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """
+    Base tests
+    """
+    pass
