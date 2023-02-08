d3911e0075a434c360962edeeccfc77c1cd90113
Mowah Chibuike <mctimmykings@gmail.com>
2023-02-08 11:43:33 +0000

    updated the file_storage.py script

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,14 +3,14 @@
 Module contains the FileStorage class
 """
 import json
-#from models.base_model import BaseModel
+
 
 class FileStorage:
     """
     FileStorage Class
     """
 
-    __file_path = "file.json"
+    __file_path = "./file.json"
     __objects = {}
 
     def all(self):
@@ -24,18 +24,14 @@ class FileStorage:
         sets in __objects the obj with key <obj class name>.id
         """
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
         """
         serializes __objects to the JSON file
         """
-        with open(self.__file_path, mode='w') as file:
-            objects_to_save = {}
-            for key, obj in self.__objects.items():
-                objects_to_save[key] = obj.to_dict()
-            json_data = json.dumps(objects_to_save)
-            file.write(json_data)
+        with open(self.__file_path, mode='w+', encoding='utf-8') as file:
+            json.dump(self.__objects, file)
 
     def reload(self):
         """
@@ -44,14 +40,9 @@ class FileStorage:
         exist, no exception should be raised
         """
         try:
-            with open(self.__file_path, mode='r') as file:
+            with open(self.__file_path, mode='r', encoding='utf-8') as file:
+                file.seek(0, 0)
                 dict_to_load = json.load(file)
-                for key, value in dict_to_load.items():
-                    class_name = value.pop("__class__")
-                    class_ = globals()[class_name]
-                    obj = class_(**value)
-                    self.__objects[{key}] = obj
+                self.__objects = dict_to_load
         except (FileExistsError, FileNotFoundError):
             pass
-
-print(globals())
